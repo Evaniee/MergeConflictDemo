02d18cd87b74ac8a649fb0575ac5d98be47df3a6
Evaniee <Evaniee7@outlook.com>
2024-10-14 19:10:19 +0100

    Add project files.

---
+++ MergeConflictDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+namespace MergeConflictDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            new Program().Menu();
+        }
+
+        public void Menu()
+        {
+            while (true)
+            {
+                try
+                {
+                    switch (Console.ReadLine().ToLower())
+                    {
+                        // TODO : Add in your own case below this comment and some code to run
+                        case "example":
+                            Console.WriteLine("This is an example of what you could do.");
+                            break;
+                        case "quit":
+                            Console.WriteLine("Goodbye!");
+                            return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
